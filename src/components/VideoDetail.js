ad8ae1950b14a70447cb3b7fc0eef8bd67d898b5
Somto M.Ugeh <smugeh@gmail.com>
2018-12-15 09:36:24 +0100

    Added title and src to iframe

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,7 +2,13 @@ import React from "react";
 
 const VideoDetail = ({ video }) => {
 	return video ? (
-		<div>
+		<div key={video.id.videoId}>
+			<div className="ui embed">
+				<iframe
+					title={video.snippet.title}
+					src={`https://www.youtube.com/embed/${video.id.videoId}`}
+				/>
+			</div>
 			<div className="ui segment">
 				<h4 className="ui header">{video.snippet.title}</h4>
 				<p> {video.snippet.description} </p>
